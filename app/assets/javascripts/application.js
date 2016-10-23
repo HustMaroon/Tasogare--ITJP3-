bde7c3e0bfd5fbc213ce7447ab6993b48a71eb2a
HustMaroon <thanhbk6294@gmail.com>
2016-10-23 20:28:24 +0700

    fix bugs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,12 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
 //= require jquery_ujs
 //= require turbolinks
 //= require star-rating
 //= require_tree .
-
-//= require jquery
 //= require bootstrap
 //= require bootstrap-datepicker
 
